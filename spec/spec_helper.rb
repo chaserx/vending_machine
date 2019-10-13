77a0365500134f9cab8241470ab63a66237f7e00
Chase Southard <chase.southard@gmail.com>
2019-10-12 21:50:07 -0400

    adds simplecov to the spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,9 @@ Aruba.configure do |config|
   config.keep_ansi = true
 end
 
+require 'simplecov'
+SimpleCov.start
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
